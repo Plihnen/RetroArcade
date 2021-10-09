39b66cb223779ab7088927debe4bc58f9e0e639b
Patrick Ihnen <plihnen@live.com>
2021-10-09 14:16:45 -0500

    Add project files.

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroArcade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetroArcade.Controllers
+{
+    public class SignInController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult SignIn(SignInModel signInModel)
+        {
+            
+            return RedirectToAction("Index", "Home" );
+        }
+    }
+}
---
+++ Models/SignInModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetroArcade.Models
+{
+    public class SignInModel
+    {
+       public string username { get; set; }
+       public string password { get; set; }
+    }
+}
